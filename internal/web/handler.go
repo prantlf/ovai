247f4c99580ba43e80fe4b32bc9c3e3f436b75d8
Ferdinand Prantl <prantlf@gmail.com>
2024-06-02 20:45:22 +0200

    fix: Allow methods specific for each handler for CORS

--- internal/web/handler.go
+++ internal/web/handler.go
@@ -8,22 +8,22 @@ import (
 	"github.com/prantlf/ovai/internal/log"
 )
 
-func IsOK(status int) bool {
+func isOK(status int) bool {
 	return status >= 200 && status < 400
 }
 
-func DisallowMethod(w http.ResponseWriter, methods []string) int {
+func disallowMethod(w http.ResponseWriter, methods []string) int {
 	w.Header().Set("Allow", strings.Join(methods, ","))
 	w.WriteHeader(http.StatusMethodNotAllowed)
 	return http.StatusMethodNotAllowed
 }
 
-func LogRequest(w http.ResponseWriter, r *http.Request) {
+func logRequest(w http.ResponseWriter, r *http.Request) {
 	log.Srv("request %s %s", r.Method, r.RequestURI)
 }
 
-func LogResponse(w http.ResponseWriter, r *http.Request, status int) {
-	if IsOK(status) {
+func logResponse(w http.ResponseWriter, r *http.Request, status int) {
+	if isOK(status) {
 		log.Srv("respond %d: %s %s", status, r.Method, r.RequestURI)
 	} else {
 		log.Dbg("fail %d: %s %s", status, r.Method, r.RequestURI)
@@ -32,13 +32,13 @@ func LogResponse(w http.ResponseWriter, r *http.Request, status int) {
 
 type LoggedHandlerFunc func(http.ResponseWriter, *http.Request) int
 
-func EnableCORS(w http.ResponseWriter, r *http.Request) {
+func enableCORS(w http.ResponseWriter, r *http.Request, methods []string) {
 	if origin := r.Header.Get("Origin"); origin != "" {
 		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Access-Control-Max-Age", "86400")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		if method := r.Header.Get("Access-Control-Request-Method"); method != "" {
-			w.Header().Set("Access-Control-Allow-Methods", "OPTIONS,POST")
+			w.Header().Set("Access-Control-Allow-Methods", strings.Join(methods, ", "))
 		}
 		if headers := r.Header.Get("Access-Control-Request-Headers"); headers != "" {
 			w.Header().Set("Access-Control-Allow-Headers", headers)
@@ -49,8 +49,8 @@ func EnableCORS(w http.ResponseWriter, r *http.Request) {
 
 func WrapHandler(fn LoggedHandlerFunc, methods []string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		LogRequest(w, r)
-		EnableCORS(w, r)
+		logRequest(w, r)
+		enableCORS(w, r, methods)
 		var status int
 		if r.Method == "OPTIONS" {
 			log.Dbg(": preflight")
@@ -59,8 +59,8 @@ func WrapHandler(fn LoggedHandlerFunc, methods []string) http.HandlerFunc {
 		} else if slices.Contains(methods, r.Method) {
 			status = fn(w, r)
 		} else {
-			status = DisallowMethod(w, methods)
+			status = disallowMethod(w, methods)
 		}
-		LogResponse(w, r, status)
+		logResponse(w, r, status)
 	}
 }
